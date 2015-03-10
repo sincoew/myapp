4cff2ad36fffbfdfc39e767d5aaa3c582fb36667
sincoew <sincoew@yahoo.com.tw>
2015-03-10 15:26:38 +0800

    first

---
+++ ksana.js
@@ -0,0 +1,48 @@
+jsonp_handler({
+ "version": "1",
+ "build": 2,
+ "title": "myapp",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/sincoew/myapp/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  547,
+  64,
+  652507,
+  145170,
+  277980,
+  382,
+  35607,
+  113502,
+  97542
+ ],
+ "filedates": [
+  "2015-03-10T07:15:05.875Z",
+  "2015-03-10T07:15:05.871Z",
+  "2015-03-10T07:15:05.949Z",
+  "2015-03-10T07:15:55.245Z",
+  "2015-03-10T07:15:05.912Z",
+  "2015-03-10T07:15:05.915Z",
+  "2015-03-10T07:15:05.814Z",
+  "2015-03-10T07:15:05.716Z",
+  "2015-03-10T07:15:18.706Z"
+ ],
+ "date": "2015-03-10T07:15:56.214Z"
+})
